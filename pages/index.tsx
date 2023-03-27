9fc6d013d89a0434f8af0f642364d5089dfff4d7
Rhama Fahri <rhamafahri18@gmail.com>
2023-03-27 21:25:54 +0700

    optimalisasi image di nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,287 @@
 import Layout from "@/components/Layout";
-
 import Styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Layout pageTitle="Home Page">
+        <Image src="/next.svg" width={200} height={200} alt="profile" />
         <h1 className={Styles["title-homepage"]}>Hello Rhama</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <Image src="/next.svg" width={200} height={200} alt="profile" />
+        <h1 className={Styles["title-homepage"]}>Hello Rhama</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <Image src="/next.svg" width={200} height={200} alt="profile" />
+        <h1 className={Styles["title-homepage"]}>Hello Rhama</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <Image src="/next.svg" width={200} height={200} alt="profile" />
+        <h1 className={Styles["title-homepage"]}>Hello Rhama</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <Image src="/next.svg" width={200} height={200} alt="profile" />
+        <h1 className={Styles["title-homepage"]}>Hello Rhama</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+          neque, consectetur animi ratione quaerat qui natus ad officia
+          asperiores ea deleniti accusantium iure culpa sunt delectus ducimus
+          autem incidunt ex fuga enim possimus! Numquam quis earum iure
+          dignissimos possimus officiis voluptate similique. Laboriosam, error!
+          Voluptas ipsa harum magnam enim debitis nemo voluptates et deleniti
+          quas nostrum facilis sit cupiditate, quia blanditiis explicabo
+          corporis commodi error necessitatibus nam voluptate voluptatum
+          sapiente alias. Ipsam labore dolores voluptates aspernatur sed eveniet
+          praesentium earum. Aliquam, perspiciatis. Vitae ipsam deserunt autem
+          voluptates magni quo dignissimos impedit cum quidem temporibus minus
+          totam facere, repellendus placeat veritatis.
+        </p>
       </Layout>
     </>
   );
