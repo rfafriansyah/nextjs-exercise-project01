af7c70b7ebe9303469edce8fd6ccb6da38d14840
Rhama Fahri <rhamafahri18@gmail.com>
2023-03-27 20:49:31 +0700

    custom halaman 404

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Oooops... </h1>
+      <h1 className="title-not-found">Halaman tidak ditemukan</h1>
+    </div>
+  );
+}
